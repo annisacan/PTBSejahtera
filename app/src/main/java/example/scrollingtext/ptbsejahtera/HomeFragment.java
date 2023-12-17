ba22b44ffb61e492feb3f8a58b7e6783cfb31067
Annisa Hasifah Cantika <89976351+annisacan@users.noreply.github.com>
2023-12-18 01:05:36 +0700

    recycler

--- app/src/main/java/example/scrollingtext/ptbsejahtera/HomeFragment.java
+++ app/src/main/java/example/scrollingtext/ptbsejahtera/HomeFragment.java
@@ -8,46 +8,35 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
-///**
-// * A simple {@link Fragment} subclass.
-// * Use the {@link HomeFragment#newInstance} factory method to
-// * create an instance of this fragment.
-// */
 public class HomeFragment extends Fragment {
 
     private TextView tabSertif, tabCV;
     private int selectedTabfragnomor = 1;
-    //cuman punya 2 jadi nilai antara 1-2
-    //deafult ke 1 karna tab deafult nya tab sertif
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         tabSertif = view.findViewById(R.id.tabSertif);
         tabCV = view.findViewById(R.id.tabCV);
 
-        //default tab
+        // Default tab
         selectTabfrag(selectedTabfragnomor);
+        getChildFragmentManager().beginTransaction()
+                .replace(R.id.fragmendihome, new SertifFragment())
+                .commit();
 
         tabSertif.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 selectedTabfragnomor = 1;
                 selectTabfrag(selectedTabfragnomor);
+                getChildFragmentManager().beginTransaction()
+                        .replace(R.id.fragmendihome, new SertifFragment())
+                        .commit();
             }
         });
 
@@ -56,45 +45,25 @@ public class HomeFragment extends Fragment {
             public void onClick(View v) {
                 selectedTabfragnomor = 2;
                 selectTabfrag(selectedTabfragnomor);
+                getChildFragmentManager().beginTransaction()
+                        .replace(R.id.fragmendihome, new CvFragment())
+                        .commit();
             }
         });
 
         return view;
     }
 
-    private void selectTabfrag (int tabNomor){
-        Fragment fragment;
-
+    private void selectTabfrag(int tabNomor) {
         if (tabNomor == 1) {
             // If tab 1 is selected, show SertifFragment
-            tabSertif.setBackgroundResource(R.drawable.round_back_putihh);
-            tabCV.setBackgroundResource(R.drawable.round_back_transparantputihh);
-            fragment = new SertifFragment();
+            tabCV.setBackgroundResource(R.drawable.round_back_putihh);
+            tabSertif.setBackgroundResource(R.drawable.round_back_transparantputihh);
+
         } else {
             // If tab 2 is selected, show CVFragment
-            tabSertif.setBackgroundResource(R.drawable.round_back_transparantputihh);
-            tabCV.setBackgroundResource(R.drawable.round_back_putihh);
-            fragment = new CvFragment();
+            tabCV.setBackgroundResource(R.drawable.round_back_transparantputihh);
+            tabSertif.setBackgroundResource(R.drawable.round_back_putihh);
         }
-        Animation slideDown = AnimationUtils.loadAnimation(getContext(), R.anim.slide_down);
-        Animation slideDownReverse = AnimationUtils.loadAnimation(getContext(), R.anim.slide_down_reverse);
-
-        replaceFragmentWithAnimation(fragment, slideDown, slideDownReverse);
-    }
-
-    private void replaceFragmentWithAnimation(Fragment fragment, Animation slideIn, Animation slideOut) {
-        FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
-
-        // Set custom animations
-        transaction.setCustomAnimations(
-                R.anim.slide_down,
-                0,
-                R.anim.slide_down_reverse,
-                0
-        );
-
-        transaction.replace(R.id.fragmendihome, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
     }
 }
--- app/src/main/java/example/scrollingtext/ptbsejahtera/SertifFragment.java
+++ app/src/main/java/example/scrollingtext/ptbsejahtera/SertifFragment.java
@@ -2,9 +2,13 @@ package example.scrollingtext.ptbsejahtera;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,6 +62,8 @@ public class SertifFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sertif, container, false);
+        View view = inflater.inflate(R.layout.fragment_sertif, container, false);
+
+        return view;
     }
 }
