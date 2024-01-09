47c1db42413f83282fb021ab3ca6a6f6198597a5
LutfiaAulia <lutfiaaulia2@gmail.com>
2024-01-09 20:21:43 +0700

    Puyeeng

--- app/src/main/java/example/scrollingtext/ptbsejahtera/SearchFragment.java
+++ app/src/main/java/example/scrollingtext/ptbsejahtera/SearchFragment.java
@@ -1,28 +1,32 @@
 package example.scrollingtext.ptbsejahtera;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class SearchFragment extends Fragment {
 
-
-   Button showMap;
+    Button showMap, notifikasi;
 
     public SearchFragment() {
     }
+
     public static SearchFragment newInstance(String param1, String param2) {
         SearchFragment fragment = new SearchFragment();
         return fragment;
@@ -40,16 +44,47 @@ public class SearchFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
 
         showMap = view.findViewById(R.id.ShowMap);
+        notifikasi = view.findViewById(R.id.notification);
 
         showMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), MapsActivity.class); // Replace MapActivity with your actual activity class
+                Intent intent = new Intent(getContext(), MapsActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        notifikasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), Tipe_Sertif.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), 0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_MUTABLE);
+
+                Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+                NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(requireContext(), "CH1")
+                        .setSmallIcon(R.drawable.baseline_notifications_none_24)
+                        .setContentText("Jangan Lupa Berprogres Hari Ini Yaa!!")
+                        .setContentTitle("Hi Sobat Edumingle")
+                        .setAutoCancel(true)
+                        .setSound(defaultSound)
+                        .setContentIntent(pendingIntent);
+
+                NotificationManager notificationManager = (NotificationManager) requireContext().getSystemService(NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel("CH1", "Hi Sobat Edumingle", NotificationManager.IMPORTANCE_DEFAULT);
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+                // Menggunakan waktu saat ini sebagai ID unik
+                long notificationId = System.currentTimeMillis();
+                notificationManager.notify((int) notificationId, notificationBuilder.build());
             }
         });
 
         return view;
     }
-
 }
